29ce505efa7a8a7b7d5ae15973a3fc068f0f0f2c
Victor <victor.badaro@fatec.sp.gov.br>
2022-01-19 13:12:56 -0300

    feat: add file to start the server and routes files

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+import express from 'express';
+import { router } from './routes';
+
+const app = express();
+const SERVER_PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(SERVER_PORT, () => console.log(`Server is running at: ${SERVER_PORT}`));
